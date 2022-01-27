9d81240f6b63ed1d139245baf047bf42215aa3fe
anushgs <anush.gs@mca.christuniversity.in>
2022-01-27 19:49:33 +0530

    Lab 7 v0.3

--- meeting.js
+++ meeting.js
@@ -1,32 +1,35 @@
 setCookies = () => {
   const bgColorVal = document.getElementById("bgColor").value;
-  if (bgColorVal !== "select color") {
+  if (bgColorVal !== "default") {
     //console.log(value);
     document.body.style.background = bgColorVal;
-    document.cookie = "bgColor=" + bgColorVal;
-  } else {
-    document.body.style.background = "#ffffff";
+    document.cookie =
+      "bgColor=" + bgColorVal + ";expires=Fri,2 Feb 2022 01:00:00 UTC;";
   }
   const fontVal = document.getElementById("font-style").value;
-  if (fontVal !== "select font") {
+  if (fontVal !== "default font") {
     document.body.style.fontFamily = fontVal;
-    document.cookie = "fontFamily=" + fontVal;
-  } else {
-    document.body.style.fontFamily = "Montserrat, sans-serif";
+    document.cookie =
+      "fontFamily=" + fontVal + ";expires=Fri,2 Feb 2022 01:00:00 UTC;";
+  }
+  if (bgColorVal !== "default" && fontVal !== "default font") {
+    localStorage.setItem("userId", "101145");
+    document.getElementById("modal-btn").className = "btn btn-dark show";
   }
 };
 
 window.onload = () => {
   if (document.cookie.length !== 0) {
-    console.log(document.cookie);
+    document.getElementById("modal-btn").className = "btn btn-primary show";
+    // console.log(document.cookie);
     let value = document.cookie.split(";");
-    console.log(value);
-    console.log(value[0].split("="));
-    console.log(value[0].split("=")[0]);
-    console.log(value[0].split("=")[1]);
-    console.log(value[1].split("="));
-    console.log(value[1].split("=")[0]);
-    console.log(value[1].split("=")[1]);
+    // console.log(value);
+    // console.log(value[0].split("="));
+    // console.log(value[0].split("=")[0]);
+    // console.log(value[0].split("=")[1]);
+    // console.log(value[1].split("="));
+    // console.log(value[1].split("=")[0]);
+    // console.log(value[1].split("=")[1]);
 
     //console.log(value[1].split("="));
 
@@ -46,7 +49,8 @@ window.onload = () => {
 
 createSession = () => {
   let value = document.getElementById("select meetings").value;
-  console.log(value);
+  //console.log(value);
+
   sessionStorage.setItem("meeting", value);
   if (value === "scrum-meeting") {
     document.getElementById("scrum-meeting").className = "scrum-meeting show";
@@ -61,4 +65,40 @@ createSession = () => {
   }
 };
 
-clearCookies = () => {};
+clearCookies = () => {
+  document.cookie = "bgColor=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie =
+    "fontFamily=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.getElementById("bgColor").value = "default";
+  document.body.style.background = "#ffffff";
+  document.body.style.fontFamily = "";
+  document.getElementById("font-style").value = "default font";
+  document.getElementById("modal-btn").className = "btn btn-dark hide";
+};
+
+showCookiesInfo = () => {
+  if (document.cookie.length !== 0) {
+    const myCookies = document.cookie;
+    const userId = localStorage.getItem("userId");
+    let cookie1 = myCookies.split(";")[0];
+    let cookie2 = myCookies.split(";")[1];
+    //console.log(cookie1);
+    //console.log(cookie2);
+    let cookie1Info = cookie1.split("=")[1];
+    let cookie2Info = cookie2.split("=")[1];
+
+    //console.log(cookie1Info);
+    //console.log(cookie2Info);
+
+    const myModal = document.getElementById("modal-body");
+    myModal.innerText =
+      "UserId: " +
+      userId +
+      "\n" +
+      "BackGround color is " +
+      cookie1Info +
+      "\n" +
+      "Font family is " +
+      cookie2Info;
+  }
+};
