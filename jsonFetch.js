aa0d67a797068b3fe45220a24001e17899b12183
anushgs <anush.gs@mca.christuniversity.in>
2022-02-03 20:20:40 +0530

    Final

--- jsonFetch.js
+++ jsonFetch.js
@@ -6,4 +6,25 @@ app.controller("meetingsCtrl", function ($scope, $http) {
     .then(function (response) {
       $scope.meetingsData = response.data.meetings;
     });
+  $scope.orderMeetingBy = function (x) {
+    $scope.myOrderBy = x;
+  };
 });
+
+app.filter("upperCase", function () {
+  return function (input) {
+    return titleCase(input);
+  };
+});
+
+function titleCase(str) {
+  var splitStr = str.toLowerCase().split(" ");
+  for (var i = 0; i < splitStr.length; i++) {
+    // You do not need to check if i is larger than splitStr length, as your for does that for you
+    // Assign it back to the array
+    splitStr[i] =
+      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  // Directly return the joined string
+  return splitStr.join(" ");
+}
